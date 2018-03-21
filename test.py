6f470408d7762d8b952e6eef29bb77b3f5312f15
ProdigyMike <tarrzn@gmail.com>
2018-03-21 12:38:23 -0400

    a

--- test.py
+++ test.py
@@ -400,7 +400,8 @@ def export_saved_model(version, path, sess=None):
         # legacy_init_op=legacy_init_op,
         # assets_collection=tf.get_collection(tf.GraphKeys.ASSET_FILEPATHS)
         # assets_collection=wordVectors  # TypeError: Asset path tensor must be a Tensor.
-        assets_collection=np.load('wordVectors.npy')
+        # assets_collection=np.load('wordVectors.npy')  # TypeError: Asset path tensor must be a Tensor.
+        assets_collection=wordVectors.shape
     )
 
     builder.save()
